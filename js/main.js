ee5b807626d3fb09518630ff80b662fb873503c3
Viacheslav <viacheslav.rybak.pb@gmail.com>
2023-03-28 18:53:31 +0300

    Form validation for: Title, price, guests, New offer form submit logic. Map update on adding new offer(marker). Filter map logic, and map update according to the filter.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,30 @@
 import { offerTypeListener, offerCheckinListener, formSubmit } from "./form.js";
-import { renderMap } from "./map.js";
+import { renderMap, reloadMapMarkers } from "./map.js";
+import { filterOffers } from "./filter.js";
 
 const form = document.querySelector('.ad-form');
 
 offerTypeListener();
 offerCheckinListener();
 
-renderMap();
+let offers = [];
+fetch("http://localhost:4000/offers")
+  .then((response) => response.json())
+  .then((data) => {
+    offers = data;
+    renderMap(offers);
+  });
 
-form.addEventListener('submit', formSubmit);
+form.addEventListener('submit', sendOffer);
+async function sendOffer(event){
+  offers = await formSubmit(event);
+}
+
+// FILTERING
+const filtersForm = document.querySelector('.map__filters');
+
+filtersForm.addEventListener('change', listenFilterForm);
+function listenFilterForm(event){
+  const filteredOffers = filterOffers(offers, event);
+  reloadMapMarkers(filteredOffers);
+}
